a30ef64ef137b4ce2b57563b353e231ac21e32f4
jackyshan <jackyshan@email.com>
2017-06-07 17:06:28 +0800

    增加对应逻辑

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
+# -*- coding: UTF-8 -*- 
+
 from flask import Flask, render_template, redirect, url_for, request
 from models import Zixun
+from database import db_session
 
 app = Flask(__name__)
 
@@ -30,10 +33,23 @@ def article_list():
     zixuns = Zixun.query.all()
     return render_template('article-list.html', zixuns = zixuns)
 
-@app.route('/article_add')
+@app.route('/article_add', methods=["GET", "POST"])
 def article_add():
+    if request.method == 'POST':
+        print '---------->>>>hello'
+        title = request.form['articletitle']
+        category = request.form['selectcategory']
+        source = request.form['sources']
+        update_time = request.form['commentdatemin']
+
+        zixun = Zixun(title, category, source, update_time)
+        db_session.add(zixun)
+        db_session.commit()
+        print '数据保存成功'
+        
+        return ''
     return render_template('article-add.html')    
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
