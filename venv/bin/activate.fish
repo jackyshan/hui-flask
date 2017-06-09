2416ea3d203919f1ffc750b5dc6ff8a5e10c010d
jackyshan <jackyshan@email.com>
2017-06-09 17:50:30 +0800

    添加gitignore配置

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jackyshan/Downloads/exercise/3.1/venv"
+set -gx VIRTUAL_ENV "/Users/jackyshan/Desktop/Project/web/flaskdemo/hui_flask/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
